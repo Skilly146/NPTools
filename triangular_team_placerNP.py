0558b583032b97f919f33e8f4895196393541531
Skilly146 <third.landon.davidosn4@gmail.com>
2022-04-04 23:43:30 -0700

    Wrote code to find x and y legs of right triangle made from equilateral triangle

--- triangular_team_placerNP.py
+++ triangular_team_placerNP.py
@@ -5,9 +5,21 @@ import sys
 
 ### Beginning of Configuration Section ###
 
-distance = 10      # Distance between each team (Group of players)
+distance = 25      # Distance between each team (Group of players)
 players = 14       # Amount of players
 team_size = 2      # Amount of players on each team
-team_distance = 2  # Distance between each teammate
+team_distance = 5  # Distance between each teammate
 
+### End of configuration section ###
 
+# Checks if amount of players can be split into even teams of TEAM_SIZE
+if players % team_size == 0:
+    num_teams = int(players / team_size)  # Calculates amount of teams for future reference
+    base_coords = []  # Initializes list for center coordinates of each team
+    curCoords = [0, 0]  # Initializes object to store current coordinates for 'for' loop
+    x_delta, y_delta = distance/2, ((3 ** 0.5) * distance)/2
+    # Calculates each leg of the right triangle made by splitting the equilateral triangle
+    # with side lengths Distance in half
+else:
+    print("{p} players can not be split into teams of {s}".format(p=players, s=team_size))
+    sys.exit()
