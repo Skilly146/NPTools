021984d539661f5283edbd85bc6a8805794f0e75
Skilly146 <third.landon.davidosn4@gmail.com>
2022-04-04 22:51:01 -0700

    Added .gitignore and triangular_team_placerNP.py

---
+++ triangular_team_placerNP.py
@@ -0,0 +1,13 @@
+# Just fill out your parameters in the labelled configuration section,
+# and then you can directly copy the printed list into NP
+
+import sys
+
+### Beginning of Configuration Section ###
+
+distance = 10      # Distance between each team (Group of players)
+players = 14       # Amount of players
+team_size = 2      # Amount of players on each team
+team_distance = 2  # Distance between each teammate
+
+
